65ba4a19dde809e7bfa0922968616c6600be8267
areebmomin <aribmomin111@gmail.com>
2024-06-16 15:33:30 +0530

    create database and table schemas

--- modules/data/build.gradle.kts
+++ modules/data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.sqlDelight)
 }
 
 kotlin {
@@ -41,6 +42,14 @@ kotlin {
 
         iosMain.dependencies {
             implementation(libs.bundles.ktor.ios)
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("WeatherUnion") {
+            packageName.set("com.areeb.weatherunion")
         }
     }
 }
