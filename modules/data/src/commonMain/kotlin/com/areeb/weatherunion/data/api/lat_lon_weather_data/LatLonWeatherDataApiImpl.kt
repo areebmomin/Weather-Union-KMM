eb4d686335dfbecfd00a6c7ee4f21f7a05c77c73
areebmomin <aribmomin111@gmail.com>
2024-06-16 13:51:25 +0530

    add api implementation

--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/api/lat_lon_weather_data/LatLonWeatherDataApiImpl.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/api/lat_lon_weather_data/LatLonWeatherDataApiImpl.kt
@@ -5,16 +5,24 @@ import com.areeb.weatherunion.core.network.apiRequest
 import com.areeb.weatherunion.core.network.httpClient
 import com.areeb.weatherunion.data.api.lat_lon_weather_data.model.LatLonWeatherDataApiResponse
 import io.ktor.client.HttpClient
+import io.ktor.http.HttpMethod
+import io.ktor.http.path
 
 class LatLonWeatherDataApiImpl(
     private val httpClient: HttpClient = httpClient(),
 ) : LatLonWeatherDataApi {
+
     override suspend fun getWeatherData(
         lat: Float,
         lon: Float
     ): ApiResponse<LatLonWeatherDataApiResponse> {
         return httpClient.apiRequest<LatLonWeatherDataApiResponse> {
-
+            url {
+                method = HttpMethod.Get
+                path("/get_weather_data")
+                parameters.append("latitude", lat.toString())
+                parameters.append("longitude", lon.toString())
+            }
         }
     }
 }
--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/api/locality_weather_data/LocalityWeatherDataApiImpl.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/api/locality_weather_data/LocalityWeatherDataApiImpl.kt
@@ -5,13 +5,20 @@ import com.areeb.weatherunion.core.network.apiRequest
 import com.areeb.weatherunion.core.network.httpClient
 import com.areeb.weatherunion.data.api.locality_weather_data.model.LocalityWeatherDataApiResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.request.parameter
+import io.ktor.http.HttpMethod
+import io.ktor.http.path
 
 class LocalityWeatherDataApiImpl(
     private val httpClient: HttpClient = httpClient(),
 ) : LocalityWeatherDataApi {
     override suspend fun getWeatherData(localityId: String): ApiResponse<LocalityWeatherDataApiResponse> {
         return httpClient.apiRequest<LocalityWeatherDataApiResponse> {
-
+            url {
+                method = HttpMethod.Get
+                path("/get_locality_weather_data")
+                parameter("locality_id", localityId)
+            }
         }
     }
 }
