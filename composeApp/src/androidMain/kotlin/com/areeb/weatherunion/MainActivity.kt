db919424e24aa01b57153764badf46f3538e8ad6
areebmomin <aribmomin111@gmail.com>
2024-06-22 21:24:02 +0530

    provide android context

--- composeApp/src/androidMain/kotlin/com/areeb/weatherunion/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/areeb/weatherunion/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // AndroidPlatformContextProvider.setContext(this)
+
         setContent {
             App()
         }
