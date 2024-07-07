39a18dd69cf5f7754fa402e9b6e6260df02a9aec
areebmomin <aribmomin111@gmail.com>
2024-07-07 13:35:45 +0530

    create api key's repository

--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/ApiCredentialRepositoryImpl.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/ApiCredentialRepositoryImpl.kt
@@ -1,20 +1,23 @@
 package com.areeb.weatherunion.data.repository
 
-class ApiCredentialRepositoryImpl : ApiCredentialRepository {
-    override fun updateWeatherUnionApiKey(apiKey: String) {
+import com.areeb.weatherunion.data.database.dao.ApiKeyDao
+import me.tatarka.inject.annotations.Inject
 
+@Inject
+class ApiCredentialRepositoryImpl(private val apiKeyDao: ApiKeyDao) : ApiCredentialRepository {
+    override fun updateWeatherUnionApiKey(apiKey: String) {
+        apiKeyDao.updateWeatherUnionApiKey(apiKey = apiKey)
     }
 
     override fun getWeatherUnionApiKey(): String {
-        TODO("Not yet implemented")
+        return apiKeyDao.getWeatherUnionApiKey()
     }
 
     override fun updateMapApiKey(apiKey: String) {
-
+        apiKeyDao.updateMapApiKey(apiKey = apiKey)
     }
 
     override fun getMapApiKey(): String {
-        TODO("Not yet implemented")
+        return apiKeyDao.getMapApiKey()
     }
-
 }
