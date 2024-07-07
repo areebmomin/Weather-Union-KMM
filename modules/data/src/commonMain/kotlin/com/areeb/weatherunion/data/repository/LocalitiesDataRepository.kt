7ac4625547a5ca757a3b6b522bcb26c099a12ff9
areebmomin <aribmomin111@gmail.com>
2024-07-07 15:26:07 +0530

    create localities data repository

--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/LocalitiesDataRepository.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/LocalitiesDataRepository.kt
@@ -1,3 +1,11 @@
 package com.areeb.weatherunion.data.repository
 
-interface LocalitiesDataRepository
+import com.areeb.weatherunion.data.locality_data.model.LocalityData
+import kotlinx.coroutines.flow.Flow
+
+interface LocalitiesDataRepository {
+    suspend fun insertLocalityDataInDB()
+    suspend fun getLocalityList(): List<LocalityData>
+    suspend fun setLastSelectedLocality(localityData: LocalityData)
+    fun getLastSelectedLocality(): Flow<String?>
+}
--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/LocalitiesDataRepositoryImpl.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/LocalitiesDataRepositoryImpl.kt
@@ -1,3 +1,29 @@
 package com.areeb.weatherunion.data.repository
 
-class LocalitiesDataRepositoryImpl : LocalitiesDataRepository
+import com.areeb.weatherunion.data.database.dao.LocalityDao
+import com.areeb.weatherunion.data.locality_data.model.LocalityData
+import com.areeb.weatherunion.data.preference.WeatherUnionPreferences
+import kotlinx.coroutines.flow.Flow
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+class LocalitiesDataRepositoryImpl(
+    private val weatherUnionPreferences: WeatherUnionPreferences,
+    private val localityDao: LocalityDao,
+) : LocalitiesDataRepository {
+    override suspend fun insertLocalityDataInDB() {
+        localityDao.insertLocalities()
+    }
+
+    override suspend fun getLocalityList(): List<LocalityData> {
+        return localityDao.getLocalityList()
+    }
+
+    override suspend fun setLastSelectedLocality(localityData: LocalityData) {
+        weatherUnionPreferences.updateLastSelectedLocalityId(localityId = localityData.localityId)
+    }
+
+    override fun getLastSelectedLocality(): Flow<String?> {
+        return weatherUnionPreferences.getLastSelectedLocalityId()
+    }
+}
