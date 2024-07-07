0ceadf9831bfe2f89817feb261a7b4b2b025234b
areebmomin <aribmomin111@gmail.com>
2024-07-07 15:33:41 +0530

    create weather data repository

--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/WeatherDataRepository.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/WeatherDataRepository.kt
@@ -1,3 +1,10 @@
 package com.areeb.weatherunion.data.repository
 
-interface WeatherDataRepository
+import com.areeb.weatherunion.core.network.ApiResponse
+import com.areeb.weatherunion.data.api.lat_lon_weather_data.model.LatLonWeatherDataApiResponse
+import com.areeb.weatherunion.data.api.locality_weather_data.model.LocalityWeatherDataApiResponse
+
+interface WeatherDataRepository {
+    suspend fun getWeatherData(lat: Float, lon: Float): ApiResponse<LatLonWeatherDataApiResponse>
+    suspend fun getWeatherData(locationId: String): ApiResponse<LocalityWeatherDataApiResponse>
+}
--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/WeatherDataRepositoryImpl.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/WeatherDataRepositoryImpl.kt
@@ -1,3 +1,25 @@
 package com.areeb.weatherunion.data.repository
 
-class WeatherDataRepositoryImpl : WeatherDataRepository
+import com.areeb.weatherunion.core.network.ApiResponse
+import com.areeb.weatherunion.data.api.lat_lon_weather_data.LatLonWeatherDataApi
+import com.areeb.weatherunion.data.api.lat_lon_weather_data.model.LatLonWeatherDataApiResponse
+import com.areeb.weatherunion.data.api.locality_weather_data.LocalityWeatherDataApi
+import com.areeb.weatherunion.data.api.locality_weather_data.model.LocalityWeatherDataApiResponse
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+class WeatherDataRepositoryImpl(
+    private val latLonWeatherDataApi: LatLonWeatherDataApi,
+    private val localityWeatherDataApi: LocalityWeatherDataApi,
+) : WeatherDataRepository {
+    override suspend fun getWeatherData(
+        lat: Float,
+        lon: Float
+    ): ApiResponse<LatLonWeatherDataApiResponse> {
+        return latLonWeatherDataApi.getWeatherData(lat = lat, lon = lon)
+    }
+
+    override suspend fun getWeatherData(locationId: String): ApiResponse<LocalityWeatherDataApiResponse> {
+        return localityWeatherDataApi.getWeatherData(localityId = locationId)
+    }
+}
