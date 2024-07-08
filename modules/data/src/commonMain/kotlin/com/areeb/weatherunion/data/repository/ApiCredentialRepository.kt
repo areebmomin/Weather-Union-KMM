6195b1944dd909e241185d6bbbaa8a0f6e9b9f45
areebmomin <aribmomin111@gmail.com>
2024-07-09 00:37:06 +0530

    improve lint issue

--- modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/ApiCredentialRepository.kt
+++ modules/data/src/commonMain/kotlin/com/areeb/weatherunion/data/repository/ApiCredentialRepository.kt
@@ -2,10 +2,7 @@ package com.areeb.weatherunion.data.repository
 
 interface ApiCredentialRepository {
     fun updateWeatherUnionApiKey(apiKey: String)
-
     fun getWeatherUnionApiKey(): String
-
     fun updateMapApiKey(apiKey: String)
-
     fun getMapApiKey(): String
 }
