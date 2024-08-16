eb251949eff7b4c1b70781c61f1ae97ab016920f
areebmomin <aribmomin111@gmail.com>
2024-08-16 19:23:30 +0530

    resolve ios database inject issue

--- modules/data/src/iosMain/kotlin/com/areeb/weatherunion/data/database/IosDatabaseDriverFactory.ios.kt
+++ modules/data/src/iosMain/kotlin/com/areeb/weatherunion/data/database/IosDatabaseDriverFactory.ios.kt
@@ -3,7 +3,9 @@ package com.areeb.weatherunion.data.database
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.areeb.weatherunion.data.WeatherUnionDatabase
+import me.tatarka.inject.annotations.Inject
 
+@Inject
 class IosDatabaseDriverFactory : DatabaseDriverFactory {
     override fun createDriver(): SqlDriver {
         return NativeSqliteDriver(WeatherUnionDatabase.Schema, "weather_union.db")
