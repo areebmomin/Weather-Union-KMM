0e68a40326909b844799a6bd826c7696a13a93a9
areebmomin <aribmomin111@gmail.com>
2024-08-16 18:54:14 +0530

    add viewmodel factory in DI

--- modules/logic/src/commonMain/kotlin/com/areeb/weatherunion/logic/di/LogicComponent.kt
+++ modules/logic/src/commonMain/kotlin/com/areeb/weatherunion/logic/di/LogicComponent.kt
@@ -1,36 +1,14 @@
 package com.areeb.weatherunion.logic.di
 
-import androidx.lifecycle.ViewModelProvider
 import com.areeb.weatherunion.logic.api_credential_screen.ApiCredentialScreenViewModel
 import com.areeb.weatherunion.logic.home_screen.HomeScreenViewModel
 import com.areeb.weatherunion.logic.location_data_screen.LocationDataScreenViewModel
-import me.tatarka.inject.annotations.IntoMap
-import me.tatarka.inject.annotations.Provides
-import kotlin.reflect.KClass
-
-typealias ViewModelFactory = ViewModelProvider.Factory
 
 interface LogicComponent {
 
-    val viewModelFactories: Map<KClass<*>, ViewModelFactory>
+    val apiCredentialScreenViewModelFactory: ApiCredentialScreenViewModel.Companion.Factory
 
-    @IntoMap
-    @Provides
-    fun apiCredentialScreenViewModelFactory(
-        factory: ApiCredentialScreenViewModel.Companion.Factory,
-    ): Pair<KClass<*>, ViewModelFactory> =
-        Pair(ApiCredentialScreenViewModel::class, factory)
+    val homeScreenViewModelFactory: HomeScreenViewModel.Companion.Factory
 
-    @IntoMap
-    @Provides
-    fun homeScreenViewModelFactory(
-        factory: HomeScreenViewModel.Companion.Factory,
-    ): Pair<KClass<*>, ViewModelFactory> = Pair(HomeScreenViewModel::class, factory)
-
-    @IntoMap
-    @Provides
-    fun locationDataScreenViewModelFactory(
-        factory: LocationDataScreenViewModel.Companion.Factory,
-    ): Pair<KClass<*>, ViewModelFactory> =
-        Pair(LocationDataScreenViewModel::class, factory)
+    val locationDataScreenViewModelFactory: LocationDataScreenViewModel.Companion.Factory
 }
