54762cbea269c6635ffbbcb9533c7240c7e1165a
areebmomin <aribmomin111@gmail.com>
2024-10-13 23:01:25 +0530

    create fake home and location data viewmodel

--- composeApp/src/commonTest/kotlin/compose/home_screen/HomeScreenTest.kt
+++ composeApp/src/commonTest/kotlin/compose/home_screen/HomeScreenTest.kt
@@ -1,5 +1,33 @@
 package compose.home_screen
 
+import com.areeb.weatherunion.core.coroutines.AppCoroutineDispatchers
+import com.areeb.weatherunion.data.fakes.api.FakeLatLonWeatherDataApiImpl
+import com.areeb.weatherunion.data.fakes.api.FakeLocalityWeatherDataApiImpl
+import com.areeb.weatherunion.data.repository.localities_data.LocalitiesDataRepositoryImpl
+import com.areeb.weatherunion.data.repository.weather_data.WeatherDataRepositoryImpl
+import com.areeb.weatherunion.logic.home_screen.usecases.WeatherDataConverterUseCase
+import com.areeb.weatherunion.logic.home_screen.viewmodel.HomeScreenViewModel
+import core.logger.TestLogger
+import data.dao.FakeLocalityDaoImpl
+import data.preference.FakeWeatherUnionPreferenceImpl
+import kotlin.test.Test
+
 class HomeScreenTest {
 
+    @Test
+    fun testHomeScreenDisplaysWeatherData() {
+        val viewModel = HomeScreenViewModel(
+            coreLogger = TestLogger(),
+            coroutineDispatchers = AppCoroutineDispatchers.coroutineDispatchers,
+            localitiesDataRepository = LocalitiesDataRepositoryImpl(
+                weatherUnionPreferences = FakeWeatherUnionPreferenceImpl(),
+                localityDao = FakeLocalityDaoImpl(),
+            ),
+            weatherDataRepository = WeatherDataRepositoryImpl(
+                latLonWeatherDataApi = FakeLatLonWeatherDataApiImpl(),
+                localityWeatherDataApi = FakeLocalityWeatherDataApiImpl(),
+            ),
+            weatherDataConverterUseCase = WeatherDataConverterUseCase(),
+        )
+    }
 }
--- composeApp/src/commonTest/kotlin/compose/location_data_screen/LocationDataScreenTest.kt
+++ composeApp/src/commonTest/kotlin/compose/location_data_screen/LocationDataScreenTest.kt
@@ -1,5 +1,33 @@
 package compose.location_data_screen
 
+import com.areeb.weatherunion.core.coroutines.AppCoroutineDispatchers
+import com.areeb.weatherunion.data.fakes.api.FakeLatLonWeatherDataApiImpl
+import com.areeb.weatherunion.data.fakes.api.FakeLocalityWeatherDataApiImpl
+import com.areeb.weatherunion.data.repository.localities_data.LocalitiesDataRepositoryImpl
+import com.areeb.weatherunion.data.repository.weather_data.WeatherDataRepositoryImpl
+import com.areeb.weatherunion.logic.home_screen.usecases.WeatherDataConverterUseCase
+import com.areeb.weatherunion.logic.location_data_screen.LocationDataScreenViewModel
+import core.logger.TestLogger
+import data.dao.FakeLocalityDaoImpl
+import data.preference.FakeWeatherUnionPreferenceImpl
+import kotlin.test.Test
+
 class LocationDataScreenTest {
 
+    @Test
+    fun testDisplaysWeatherData() {
+        val viewModel = LocationDataScreenViewModel(
+            coreLogger = TestLogger(),
+            coroutineDispatchers = AppCoroutineDispatchers.coroutineDispatchers,
+            localitiesDataRepository = LocalitiesDataRepositoryImpl(
+                weatherUnionPreferences = FakeWeatherUnionPreferenceImpl(),
+                localityDao = FakeLocalityDaoImpl(),
+            ),
+            weatherDataRepository = WeatherDataRepositoryImpl(
+                latLonWeatherDataApi = FakeLatLonWeatherDataApiImpl(),
+                localityWeatherDataApi = FakeLocalityWeatherDataApiImpl(),
+            ),
+            weatherDataConverterUseCase = WeatherDataConverterUseCase(),
+        )
+    }
 }
